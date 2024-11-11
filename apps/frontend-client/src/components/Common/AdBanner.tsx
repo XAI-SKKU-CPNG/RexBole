eaa82a9916cf606ba98c5709ac71994265e02205
Jimin Ha <jimin9038@g.skku.edu>
2024-11-11 04:14:42 +0000

    feat(fe): show explanation as modal

--- apps/frontend-client/src/components/Common/AdBanner.tsx
+++ apps/frontend-client/src/components/Common/AdBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AdBanner: React.FC = () => {
   return (
-    <div className="text-center my-4">
+    <div className="text-center my-4 bg-white">
       <h2 className="text-2xl font-bold">지금 이 상품이 필요하신가요?</h2>
       <span className="text-gray-500">광고</span>
     </div>
